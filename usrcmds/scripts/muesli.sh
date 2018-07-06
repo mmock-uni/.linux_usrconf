dc0dc686ae125230be023f73a94f0d8f8857898c
ekzyis <r.gill@stud.uni-heidelberg.de>
2018-07-06 14:14:28 +0200

    Migrated __muesli as own file inside new folder /usrcmds/scripts/
    Migrated the function __muesli inside webfunc to muesli.sh inside usrcmds/scripts/ since it behaves like a "proper script" and should not "just" be a function.
    (Also I want some executable files in my repository because their black icon looks really "programmy" and cool :grin:)

---
+++ usrcmds/scripts/muesli.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script logs into muesli and greps the current points, saves them and outputs them on the console with vim.
+if [[ ! $# -eq 2 ]]; then
+  echo "Usage: muesli LECTURE-MÜSLI-WEBPAGE PATH_TO_SAVE"
+  exit 1
+fi
+echo "--- This is muesli v1.0 ---"
+echo "Please enter your login credentials for Müsli @ mathi.uni-heidelberg.de"
+read -p "Email: " mail
+read -s -p "Password: " pw
+printf "\n"
+# Initialize cookie jar
+curl --silent --cookie-jar cjar --output /dev/null https://muesli.mathi.uni-heidelberg.de/user/login
+# Login
+curl --silent --cookie cjar --cookie-jar cjar --output /dev/null --data "email=$mail" --data "password=$pw" -o /dev/null -L https://muesli.mathi.uni-heidelberg.de/user/login
+# Get the actual website and parse it
+curl --silent --cookie cjar -L "$1" | grep -oE "Resultate für \w+(\s*\w*)*|[0-9]+\.[0-9]\/[0-9]+\.[0-9]|-\/[0-9]+\.[0-9]|Übungsblatt [0-9]+|Übungszettel [0-9]+|[0-9]+%" > "$2"
+rm cjar
+echo "Opening vim to show results..."
+vim "$2"

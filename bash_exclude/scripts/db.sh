2f8ae5e288a37308aad0a6820b95001f496b1186
ekzyis <r.gill@stud.uni-heidelberg.de>
2018-11-14 02:06:49 +0100

    Implemented printing to terminal instead of opening browser
    Departure time, arrival time, duration and product is now printed to terminal.
    Each row represents one connection.

--- bash_exclude/scripts/db.sh
+++ bash_exclude/scripts/db.sh
@@ -21,16 +21,25 @@ if [ "$TIME" = "" ]; then
   TIME=$(date "+%H:%M:%S")
 fi
 
-echo "POST: start=$START destination=$DEST date=$DATE time=$TIME"
-
 FORMACTION="https://reiseauskunft.bahn.de/bin/query.exe/dn?ld=36251&protocol=https:&seqnr=1&ident=fq.026695251.1539027464&rt=1&OK"
 DEFAULTDATA="HWAI%3DQUERY%21rit=no&queryPageDisplayed=yes&HWAI%3DQUERY%21displayed=yes&HWAI%3DJS%21ajax=yes&HWAI%3DJS%21js=yes&REQ0JourneyStopsS0A=255&ignoreTypeCheck=yes&REQ0JourneyStopsS0a=131072&REQ0JourneyStopsZ0A=255&REQ0JourneyStopsZ0o=8&REQ0JourneyStopsZ0a=131072&REQ1JourneyDate=&REQ1JourneyTime=&REQ1HafasSearchForw=1&REQ0JourneyRevia=yes&HWAI%3DQUERY%24via%240%21number=0&REQ0JourneyStops1ID=&REQ0JourneyStops2ID=&HWAI%3DQUERY%24via%241%21number=0&REQ1JourneyStops1ID=&REQ1JourneyStops2ID=&HWAI%3DQUERY%21prodAdvanced=0&existOptimizePrice=1&REQ0HafasOptimize1=0%3A1&existProductNahverkehr=1&HWAI%3DQUERY%24PRODUCTS%240_0%21show=yes&HWAI%3DQUERY%24PRODUCTS%240_0%21show=yes&advancedProductMode=yes&REQ0JourneyProduct_prod_section_0_0=1&REQ0JourneyProduct_prod_section_0_1=1&REQ0JourneyProduct_prod_section_0_2=1&REQ0JourneyProduct_prod_section_0_3=1&REQ0JourneyProduct_prod_section_0_4=1&REQ0JourneyProduct_prod_section_0_5=1&REQ0JourneyProduct_prod_section_0_6=1&REQ0JourneyProduct_prod_section_0_7=1&REQ0JourneyProduct_prod_section_0_8=1&REQ0JourneyProduct_prod_section_0_9=1&REQ0JourneyProduct_opt_section_0_list=0%3A0000&existProductAutoReturn=yes&REQ0HafasChangeTime=0%3A1&existIntermodalDep_enable=yes&REQ0JourneyDep__enable=Foot&existIntermodalDest_enable=yes&REQ0JourneyDest__enable=Foot&HWAI%3DQUERY%21hideExtInt=no&REQ0JourneyDep_Foot_minDist=0&REQ0JourneyDest_Foot_minDist=0&REQ0JourneyDep_Foot_maxDist=2000&REQ0JourneyDest_Foot_maxDist=2000&REQ0JourneyDep_Bike_minDist=0&REQ0JourneyDest_Bike_minDist=0&REQ0JourneyDep_Bike_maxDist=5000&REQ0JourneyDest_Bike_maxDist=5000&REQ0JourneyDep_KissRide_minDist=2000&REQ0JourneyDest_KissRide_minDist=2000&REQ0JourneyDep_KissRide_maxDist=50000&REQ0JourneyDest_KissRide_maxDist=50000&travelProfile=&traveller_Nr=1&REQ0Tariff_TravellerType.1=E&REQ0Tariff_TravellerReductionClass.1=0&REQ0Tariff_TravellerAge.1=&REQ0Tariff_Class=2&existOptionBits=yes&rtMode=12&start=Suchen"
 
-curl \
+RESPONSE=$(curl \
 --data "REQ0JourneyStopsS0G=$START" \
 --data "REQ0JourneyStopsZ0G=$DEST" \
 --data "REQ0JourneyTime=$TIME" \
 --data "REQ0JourneyDate=$DATE" \
 --data "REQ0HafasSearchForw=0" \
---data "$DEFAULTDATA" "$FORMACTION" -o db.html -s
-( chromium-browser db.html & ) > /dev/null 2>&1
+--data "$DEFAULTDATA" "$FORMACTION" -s)
+
+DEPARTURES=$(echo "$RESPONSE" | grep -zoP '(?<=<td class="time" style="min-width:130px;">)(\n)?[0-9]+:[0-9]+' | tr -d '\0' | tr '\n' ' ')
+ARRIVALS=$(echo "$RESPONSE" | grep -zoP '(?<=<td class="time">)(\n)?[0-9]+:[0-9]+' | tr -d '\0' | tr '\n' ' ')
+DURATIONS=$(echo "$RESPONSE" | grep -zoP '(?<=<td class="duration lastrow" rowspan="2">)(\n)?[0-9]:[0-9]+' | tr -d '\0' | tr '\n' ' ')
+PRODUCTS=$(echo "$RESPONSE" | grep -zoP '(?<=<td class="products lastrow" rowspan="2">)(\n)?[A-Z]+' | tr -d '\0' | tr '\n' ' ') 
+read -rd '' -a dep <<< "$DEPARTURES"
+read -rd '' -a arr <<< "$ARRIVALS"
+read -rd '' -a dur <<< "$DURATIONS"
+read -rd '' -a pro <<< "$PRODUCTS"
+for i in "${!dep[@]}"; do
+  echo "${dep[$i]} ${arr[$i]} ${dur[$i]} ${pro[$i]}"
+done | column -c 1

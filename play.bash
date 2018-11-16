1923ccd9b0e423f8484fb51e0d8ebd297fe84502
ekzyis <r.gill@stud.uni-heidelberg.de>
2018-11-16 18:13:33 +0100

    Fix queueing of non audio files

--- play.bash
+++ play.bash
@@ -7,5 +7,7 @@ play() {
   for a in "$@"; do
     regex+=".*$a"
   done
-  ( find /media/ekzyis/HDD/ekzyis/Musik -iregex ".*\(pls\|ini\|jpg\|db\)$" -prune -o -iregex "$regex.*" -print0 | xargs -0 xplayer --enqueue > /dev/null 2>&1 & )
+  findCmd="find /media/ekzyis/HDD/ekzyis/Musik -type f -iregex '$regex.*' ! -iregex '.*.\(pls\|ini\|jpg\|db\|ico\)'"
+  eval "$findCmd"
+  ( eval "$findCmd" -print0 | xargs -0 xplayer --enqueue > /dev/null 2>&1 & )
 }
